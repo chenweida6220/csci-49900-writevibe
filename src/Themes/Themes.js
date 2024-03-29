3eb42769746e30f15d65399e2e32aeb647b676c4
redfumo <92561677+redfumo@users.noreply.github.com>
2024-03-28 22:37:35 -0400

    add customization menu

--- src/Themes/Themes.js
+++ src/Themes/Themes.js
@@ -1,5 +1,23 @@
 import React, { useState } from "react";
 import './Themes.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import PaletteIcon from '@mui/icons-material/Palette';
+
+//css for settings window
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    bgcolor: 'background.paper',
+    border: '2px solid #EEE',
+    borderRadius: 2,
+    boxShadow: 12,
+    p: 3, //padding
+};
 
 const Themes = ({ onChangeTheme }) => {
 
@@ -11,33 +29,69 @@ const Themes = ({ onChangeTheme }) => {
         onChangeTheme(newFormat);
     };
 
+    //for settings menu
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
         <div className="themeTypes">
-            <button 
-                className={format === 'space' ? 'active' : ''}
-                onClick={() => handleThemes('space')}
+            <PaletteIcon 
+                id="paletteIcon" 
+                onClick={handleOpen}
+                fontSize='large'
+                sx={{ color: 'white' }}
             >
-                Space
-            </button>
-            <button 
-                className={format === 'warm' ? 'active' : ''}
-                onClick={() => handleThemes('warm')}
+            </PaletteIcon>  
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
             >
-                Warm
-            </button>
-            <button 
-                className={format === 'rain' ? 'active' : ''}
-                onClick={() => handleThemes('rain')}
-            >
-                Rain
-            </button>
-            <button 
-                className={format === 'cafe' ? 'active' : ''}
-                onClick={() => handleThemes('cafe')}
-            >
-                Cafe
-            </button>
+                <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Settings
+                </Typography>
+
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <label>Preset Themes: </label>
+                    <select name="preset" id="preset">
+                        <option 
+                            value="space"
+                            className={format === 'space' ? 'active' : ''}
+                            onClick={() => handleThemes('space')}
+                        >
+                            Space
+                        </option>
+                        <option 
+                            value="warm"
+                            className={format === 'warm' ? 'active' : ''}
+                            onClick={() => handleThemes('warm')}
+                        >
+                            Warm
+                        </option>
+                        <option 
+                            value="rain"
+                            className={format === 'rain' ? 'active' : ''}
+                            onClick={() => handleThemes('rain')}
+                        >
+                            Rain
+                        </option>
+                        <option 
+                            value="cafe"
+                            className={format === 'cafe' ? 'active' : ''}
+                            onClick={() => handleThemes('cafe')}
+                        >
+                            Cafe
+                        </option>
+                    </select>
+                    <br />
+                </Typography>
+
+                </Box>
+            </Modal>
         </div>
     )
 };
